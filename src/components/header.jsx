80ee08fe3e322e7f88058aa87b9192e1161e791c
Sophie Théréau <sophiethereau@hotmail.fr>
2024-09-05 15:33:41 +0200

    logout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,69 +1,100 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
+import axios from 'axios';
 import '../style/Header.css';
 
 const Header = () => {
-
   const { isAuthenticated, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    console.log('Déconnexion en cours...');
+    try {
+        const token = localStorage.getItem('token');
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        };
+        await axios.post('http://localhost:8000/api/logout', {}, config);
+        logout();
+        navigate('/');
+    } catch (error) {
+        console.error('Erreur lors de la déconnexion', error);
+    }
+  };
 
   return (
-    <>
-      <header>
-        <div class="navbar">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
-                <ul
-                    tabindex="0"
-                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><Link to="/" className="">Accueil</Link></li>
-                    {isAuthenticated && <li><Link to="/" className="">Mes transactions</Link></li>}
-                    {isAuthenticated && <li><Link to="/" className="">Mon compte</Link></li>}
-                    {isAuthenticated && <li><Link to="/" className="">Me déconnecter</Link></li>}
-                    {!isAuthenticated && <li><Link to="/login" className="">Me connecter</Link></li>}
-                    {!isAuthenticated && <li><Link to="/register" className="">Créer un compte</Link></li>}
-                </ul>
-                </div>
-                <Link to="/" className="btn-ghost">
-                    <img
-                        src="/images/MonTrésor-removebg-preview.png"
-                        alt="Logo"
-                        className="h-14 lg:h-19"
-                    />
-                </Link>
+    <header>
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal px-1">
-                    <li><Link to="/" className="">Accueil</Link></li>
-                    {isAuthenticated && <li><Link to="/" className="">Mes transactions</Link></li>}
-                    {isAuthenticated && <li><Link to="/" className="">Mon compte</Link></li>}
-                </ul>
-            </div>
-            <div class="navbar-end">
-                <ul class="menu menu-horizontal px-1">
-                    {isAuthenticated && <li><Link to="/" className="">Me déconnecter</Link></li>}
-                    {!isAuthenticated && <li><Link to="/login" className="">Me connecter</Link></li>}
-                    {!isAuthenticated && <li><Link to="/register" className="">Créer un compte</Link></li>}
-                </ul>
-            </div>
-            </div>
-      </header>
-    </>
+            <ul
+              tabIndex="0"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li><Link to="/" className="">Accueil</Link></li>
+              {isAuthenticated && <li><Link to="/" className="">Mes transactions</Link></li>}
+              {isAuthenticated && <li><Link to="/" className="">Mon compte</Link></li>}
+              {isAuthenticated && (
+                <li>
+                  <button onClick={handleLogout} className="">
+                    Me déconnecter
+                  </button>
+                </li>
+              )}
+              {!isAuthenticated && <li><Link to="/login" className="">Me connecter</Link></li>}
+              {!isAuthenticated && <li><Link to="/register" className="">Créer un compte</Link></li>}
+            </ul>
+          </div>
+          <Link to="/" className="btn-ghost">
+            <img
+              src="/images/MonTrésor-removebg-preview.png"
+              alt="Logo"
+              className="h-14 lg:h-19"
+            />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to="/" className="">Accueil</Link></li>
+            {isAuthenticated && <li><Link to="/" className="">Mes transactions</Link></li>}
+            {isAuthenticated && <li><Link to="/" className="">Mon compte</Link></li>}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1">
+            {isAuthenticated && (
+              <li>
+                <button onClick={handleLogout} className="">
+                  Me déconnecter
+                </button>
+              </li>
+            )}
+            {!isAuthenticated && <li><Link to="/login" className="">Me connecter</Link></li>}
+            {!isAuthenticated && <li><Link to="/register" className="">Créer un compte</Link></li>}
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 };
 
 export default Header;
-
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, UseNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
 import '../style/Login.css';
 
